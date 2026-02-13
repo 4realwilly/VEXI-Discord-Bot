665067d04413efdbe99583f6c0b014c863d9548e
William Holley <wholley123@icloud.com>
2026-02-13 15:03:02 -0600

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,26 +1,33 @@
-
 const fs = require('fs');
 const path = require('path');
 
-module.exports = async (client) => {
+const eventHandler = async (client) => {
     const eventsPath = path.join(__dirname, '../events');
 
     const loadEvents = (dir) => {
         const files = fs.readdirSync(dir);
+
         for (const file of files) {
             const fullPath = path.join(dir, file);
+
             if (fs.statSync(fullPath).isDirectory()) {
                 loadEvents(fullPath);
             } else if (file.endsWith('.js')) {
                 const event = require(fullPath);
 
                 if (dir.includes('process')) {
-                    process.on(event.name, (...args) => event.execute(...args, client));
+                    process.on(event.name, (...args) =>
+                        event.execute(...args, client)
+                    );
                 } else {
                     if (event.once) {
-                        client.once(event.name, (...args) => event.execute(...args, client));
+                        client.once(event.name, (...args) =>
+                            event.execute(...args, client)
+                        );
                     } else {
-                        client.on(event.name, (...args) => event.execute(...args, client));
+                        client.on(event.name, (...args) =>
+                            event.execute(...args, client)
+                        );
                     }
                 }
             }
@@ -28,4 +35,9 @@ module.exports = async (client) => {
     };
 
     loadEvents(eventsPath);
+
+    console.log('Events loaded.');
 };
+
+module.exports = eventHandler;
+module.exports.startup = true; // ✅ Important
