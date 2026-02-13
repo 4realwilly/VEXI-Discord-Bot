c4c6e08172b03adb242460c983dc18fe23b52842
William Holley <wholley123@icloud.com>
2026-02-13 15:04:50 -0600

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
-
+// events/client/clientReady.js
 module.exports = {
-    name: 'ready',
+    name: 'clientReady', // 🔹 updated
     once: true,
     execute(client) {
         console.log(`Logged in as ${client.user.tag}`);
